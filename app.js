13e4c6de7fb4d658bbb10ff1c15de61f748c5b0f
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-11-26 19:53:10 +0530

    Clean Up Code

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    const body = [];
+const routes = require('./routes.js');
 
-    if (url === '/') {
-        fs.readFile('message.txt', { encoding: 'utf-8' }, (error, data) => {
-            if (error) {
-                console.log(error);
-                res.end();
-            } else {
-                console.log(data);
-                res.write('<html>');
-                res.write('<head><title>Enter Message</title></head>');
-                res.write('<body>');
-                res.write(`<p>${data}</p>`)
-                res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>');
-                res.write('</body>');
-                res.write('</html>');
-                return res.end();
-            }
-        });
-    }
-    else if (url === '/message' && method === 'POST') {
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message, () => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-});
+console.log(routes.someText)
+
+const server = http.createServer(routes.handler)
 
 server.listen(3000);
