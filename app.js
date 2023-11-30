1c67a38ddb8126d828d406c7333c9aaf45cbcfd3
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-11-30 15:51:00 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser')
 
@@ -6,15 +7,20 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
+const contactusRoutes = require('./routes/contactus');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
+app.use(contactusRoutes);
+app.use(successRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found </h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 //  /admin/add-product  -> GET Requests
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" placeholder="Enter title" name="title"/> <input type="number" placeholder="Enter size" name="number"/> <button type="submit">Add Product </button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 })
 
 //  /admin/add-product  -> POST Requests
